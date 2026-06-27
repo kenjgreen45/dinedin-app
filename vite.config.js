06f16c78d5903a8c8e97a23ab7a1678fd907828f
kenjgreen45 <ken.sub.bills@gmail.com>
2026-06-27 16:38:41 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         app: resolve(__dirname, 'app/index.html'),
+        restaurants: resolve(__dirname, 'restaurants/index.html'),
       },
     },
   },
